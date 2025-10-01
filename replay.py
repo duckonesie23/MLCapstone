1de2e8b0b283188657e15da481cc165f096f4276
mbondoc <51180797+duckonesie23@users.noreply.github.com>
2025-10-01 14:15:12 -0400

    l,

--- replay.py
+++ replay.py
@@ -1,3 +1,4 @@
+from select import select
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -5,7 +6,33 @@ import os
 
 environments = ["CartPole-v1","MountainCar-v0","LunarLander-v3","BipedalWalker-v3","CarRacing-v3"]
 
-env = gym.make(environments[1], render_mode="human")
+filelist = os.listdir(f"logs")
+
+def menu():
+    print("Which model would you like to view?")
+    for i in range(len(environments)):
+        print(f"{i}: {environments[i]}" )
+    listModels(int(input()))
+
+def listModels(selection):
+    modelVersions = []
+    print(environments[selection].lower())
+    try:
+        for file in filelist:
+            if environments[selection].lower() in file:
+                modelVersions.append(file)
+        for i in range(len(modelVersions)):
+            print(f"{i}: {modelVersions[i]}")
+            
+    except Exception as e:
+        print(e)
+
+
+    
+
+menu()
+
+'''env = gym.make(environments[1], render_mode="human")
 model = PPO.load(f"logs/ppo_{environments[1]}.zip",env=env)
 
 episode_rewards = []
@@ -21,3 +48,11 @@ for _ in range(5):
         print(f"Episode {len(episode_rewards)} reward: {total_reward}")
         total_reward = 0
         obs, _ = env.reset()
+
+try:
+        for file in filelist:
+            if environments[selection].lower() in file:
+                modelVersions.append()
+    except:
+        print("error!")
+        '''
